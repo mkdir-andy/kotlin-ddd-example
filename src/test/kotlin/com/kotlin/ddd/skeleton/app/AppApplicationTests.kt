344e63659b21cc80df174af257c38e03a7d7bba8
mrandycodes <im.andi.more@gmail.com>
2024-09-06 16:09:28 +0200

    Add initial setup

---
+++ src/test/kotlin/com/kotlin/ddd/skeleton/app/AppApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.kotlin.ddd.skeleton.app
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class AppApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
